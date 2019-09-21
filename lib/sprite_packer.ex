09654e1f5e709aaf6effe17d9f55d726daa44ef6
anandu <anandu@gamezop.co>
2019-09-21 06:37:46 +0530

    init

---
+++ lib/sprite_packer.ex
@@ -0,0 +1,18 @@
+defmodule SpritePacker do
+  @moduledoc """
+  Documentation for SpritePacker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpritePacker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sprite_packer_test.exs
@@ -0,0 +1,8 @@
+defmodule SpritePackerTest do
+  use ExUnit.Case
+  doctest SpritePacker
+
+  test "greets the world" do
+    assert SpritePacker.hello() == :world
+  end
+end
